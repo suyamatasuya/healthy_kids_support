c7567859887c8cc632cd1ad8a527d90b1093f794
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2024-08-18 14:44:23 +0900

    プロフィール情報の取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show, :update]
   
-  resources :users, only: [:create]
+  # メールアドレスでユーザーを検索するエンドポイント
+  get 'users/find_by_email', to: 'users#find_by_email'
 
   devise_for :users, skip: [:registrations], controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   get '/csrf_token', to: 'application#csrf_token'
-
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
 end
